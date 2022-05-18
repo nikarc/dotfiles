62fe350ddeff9f3adaf1b9c6dbc3db174f3c0848
Nick Arcuri <nickarcuri@protonmail.com>
2022-05-18 11:38:17 -0400

    Makes telescope wider

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,19 +18,20 @@ Website: https://github.com/brainfucksec/neovim-lua
 -----------------------------------------------------------
 -- Import Lua modules
 -----------------------------------------------------------
-require('settings')                 -- settings
-require('keymaps')                  -- keymaps
-require('plugins/packer')           -- plugin manager
--- require('plugins/nvim-tree')	      -- file manager
-require('plugins/feline')           -- statusline
--- require('plugins/lualine')
-require('plugins/nvim-cmp')         -- autocomplete
+require('settings') -- settings
+require('keymaps') -- keymaps
+
+require('plugins/packer') -- plugin manager
+require('plugins/feline') -- statusline
+require('plugins/nvim-cmp') -- autocomplete
 require('plugins/null-ls')
-require('plugins/nvim-autopairs')   -- autopairs
-require('plugins/nvim-lspconfig')   -- LSP settings
-require('plugins/vista')            -- tag viewer
-require('plugins/nvim-treesitter')  -- tree-sitter interface
-require('plugins/alpha-nvim')       -- Start page plugin
-require('plugins/bufferline-nvim')  -- Buffer tabs
-require('plugins/searchbox-nvim')   -- Styled search box
--- require('plugins/renamer')
+require('plugins/nvim-autopairs') -- autopairs
+require('plugins/nvim-lspconfig') -- LSP settings
+require('plugins/vista') -- tag viewer
+require('plugins/nvim-treesitter') -- tree-sitter interface
+require('plugins/alpha-nvim') -- Start page plugin
+require('plugins/bufferline-nvim') -- Buffer tabs
+require('plugins/searchbox-nvim') -- Styled search box
+require('plugins/lua-dev')
+require('plugins/renamer')
+require('plugins/telescope-nvim')
---
+++ .config/nvim/lua/plugins/telescope-nvim.lua
@@ -0,0 +1,10 @@
+require('telescope').setup({
+  defaults = {
+    layout_config = {
+      horizontal = { width = 0.95 }
+      -- other layout configuration here
+    },
+    -- other defaults configuration here
+  },
+  -- other configuration values here
+})
