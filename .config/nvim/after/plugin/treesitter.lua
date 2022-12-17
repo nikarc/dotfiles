a59cec5dcbff460248afe8686749a7dc1474a4ec
Nick Arcuri <nickarcuri@protonmail.com>
2022-12-17 09:25:04 -0500

    Treesitter indent and buf nav keymaps

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -15,9 +15,13 @@ require'nvim-treesitter.configs'.setup {
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-  -- indent = {
-  --     enable = true
-  -- },
+  indent = {
+      enable = true,
+      disable = {
+          'python',
+          'c'
+      }
+  },
 
   highlight = {
     -- `false` will disable the whole extension
