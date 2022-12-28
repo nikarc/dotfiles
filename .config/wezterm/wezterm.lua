3198c0a5aa4fcbabedfa44ba9fd58feb53b654bb
Nicholas Arcuri <nickarcuri@protonmail.com>
2022-12-27 20:51:49 -0500

    :beers: Various visual debauchery

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,4 +4,7 @@ return {
   font = wezterm.font 'Fira Code',
   window_background_opacity = 0.85,
   window_background_tint = 0,
+  cursor_blink_rate = 800,
+  default_cursor_style = 'BlinkingBlock',
+  animation_fps = 60,
 }
