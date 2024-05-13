13c7c4c903ca3385ab9d34649e0c710aa8777913
Nick Arcuri <nickarcuri@protonmail.com>
2024-05-13 09:20:21 -0400

    :gear: Wezterm tab next to current

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,15 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
+function ACTIVE_TAB_IDX(mux_win)
+   for _, item in ipairs(mux_win:tabs_with_info()) do
+      -- wezterm.log_info('idx: ', idx, 'tab:', item)
+      if item.is_active then
+         return item.index
+      end
+   end
+end
+
 return {
   animation_fps = 60,
   cursor_blink_rate = 800,
@@ -19,6 +28,19 @@ return {
       key = 'k',
       mods = 'SUPER',
       action = act.ClearScrollback 'ScrollbackAndViewport',
+    },
+    {
+      key = 't',
+      mods = 'CTRL|SHIFT',
+      -- https://github.com/wez/wezterm/issues/909
+      action = wezterm.action_callback(function(win, pane)
+        local mux_win = win:mux_window()
+        local idx = ACTIVE_TAB_IDX(mux_win)
+        -- wezterm.log_info('ACTIVE_TAB_IDX: ', idx)
+        local tab = mux_win:spawn_tab({})
+        -- wezterm.log_info('movetab: ', idx)
+        win:perform_action(wezterm.action.MoveTab(idx+1), pane)
+      end),
     },
   }
 }
