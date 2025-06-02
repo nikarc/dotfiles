0a8c095a43f48a7ad7f36dcb90bf272b8605fb8e
Nicholas Arcuri <nickarcuri@protonmail.com>
2025-06-02 15:01:53 -0400

    wezterm: Linux font size

--- .config/wezterm/linux.lua
+++ .config/wezterm/linux.lua
@@ -5,5 +5,6 @@ local wezterm = require 'wezterm'
 return {
   -- font = wezterm.font 'agave Nerd Font',
   font = wezterm.font 'Iosevka Nerd Font',
+  font_size = 18,
     -- font = wezterm.font 'JetBrains Mono Nerd Font',
 }
