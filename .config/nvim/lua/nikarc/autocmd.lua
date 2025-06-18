35f34085abb591a09f40ece1168a95612e4a2135
Nicholas Arcuri <nickarcuri@protonmail.com>
2025-06-18 09:04:27 -0400

    nvim: fix autocmd

--- .config/nvim/lua/nikarc/autocmd.lua
+++ .config/nvim/lua/nikarc/autocmd.lua
@@ -1,8 +1,5 @@
 -- Show errors in a floating window on hover
 vim.api.nvim_create_autocmd("CursorHold", {
-  config = {
-      updatetime = 1000,
-  },
   callback = function ()
     vim.diagnostic.open_float()
   end
