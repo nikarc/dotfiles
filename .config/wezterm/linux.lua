62135444bfc279316ced91b3af5ca1dcefd39ad9
Nick Arcuri <nickarcuri@pm.me>
2025-11-18 10:03:33 -0500

    hypr: config

--- .config/wezterm/linux.lua
+++ .config/wezterm/linux.lua
@@ -6,5 +6,6 @@ return {
   -- font = wezterm.font 'agave Nerd Font',
   font = wezterm.font 'Iosevka Nerd Font',
   font_size = 20,
+  window_background_opacity = 0.5,
     -- font = wezterm.font 'JetBrains Mono Nerd Font',
 }
